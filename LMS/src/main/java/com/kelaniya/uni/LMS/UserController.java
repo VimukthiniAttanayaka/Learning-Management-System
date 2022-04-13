43aa0351462260758d3b9bed2bdcb55107b048ed
chathuranga566 <lahiruchathuranga19981214@gmail.com>
2022-04-13 10:11:51 +0530

    add @CrossOrigin to UserController class

--- LMS/src/main/java/com/kelaniya/uni/LMS/UserController.java
+++ LMS/src/main/java/com/kelaniya/uni/LMS/UserController.java
@@ -1,12 +1,10 @@
 package com.kelaniya.uni.LMS;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class UserController {
 
     private UserRepository userRepository;
