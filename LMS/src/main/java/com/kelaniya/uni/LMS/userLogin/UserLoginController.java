49cb9110ff5e5273165e298f2087a96d27f12c31
Poornajith <chiranthapoornajith@gmail.com>
2022-04-23 23:11:14 +0530

    modify user login coltroller

--- LMS/src/main/java/com/kelaniya/uni/LMS/userLogin/UserLoginController.java
+++ LMS/src/main/java/com/kelaniya/uni/LMS/userLogin/UserLoginController.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Objects;
 
-@Controller
+@RestController
 public class UserLoginController {
     @Autowired
     private UserLoginService userLoginService;
