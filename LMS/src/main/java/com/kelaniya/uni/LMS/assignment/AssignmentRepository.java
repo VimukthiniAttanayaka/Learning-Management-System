7dc8c1ad3562fc74c42faf888cfe235350c26820
Poornajith <chiranthapoornajith@gmail.com>
2022-05-17 16:56:33 +0530

    email sending via sendgrid api

--- LMS/src/main/java/com/kelaniya/uni/LMS/assignment/AssignmentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.kelaniya.uni.LMS.assignment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssignmentRepository extends JpaRepository<Assignment, Integer> {
-}
--- LMS/src/main/java/com/kelaniya/uni/LMS/userLogin/UserLoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kelaniya.uni.LMS.userLogin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserLoginRepository extends JpaRepository <UserLoginDomain, Long> {
-    UserLoginDomain findByEmailAndPassword(String email, String password);
-}
